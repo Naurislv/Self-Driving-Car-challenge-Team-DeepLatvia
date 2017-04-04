4d6287ca3a4e47d7896cec117281244e6860ecf0
Aleksandrs Levinskis <a.levinskis@gmail.com>
2017-04-04 13:21:12 -0700

    Preparing catkin workspace

---
+++ ROS/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/alex/DiDi_Challenge/Team-DeepLatvia-Self-Driving-Car-challenge/ROS/catkin_ws/src
+# Build directory: /mnt/hgfs/DiDi_Cahllenge/Team-DeepLatvia-Self-Driving-Car-challenge/ROS/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
